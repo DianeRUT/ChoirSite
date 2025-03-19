2aee0cb27b0a1ccaf76e09567f17289c11f16fda
Ilysse200 <ilysseted500@gmail.com>
2025-03-19 23:58:59 +0200

    added commision page and its designs

--- Choir-Frontend/src/App.jsx
+++ Choir-Frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import FamilyGrid from './FamilyGrid'
+import Commission from './Commission'
 function App() {
   return (
     <BrowserRouter>
     <Routes>
         <Route path='/' index element={<FamilyGrid/>}/>
+        <Route path='/commission' index element={<Commission/>}/>
     </Routes>
     </BrowserRouter>
   )
