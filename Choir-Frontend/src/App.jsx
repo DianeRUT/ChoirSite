f2603e34c37223a04ad34b744210eb1a2772db87
DianeRUT <ingabirerdianna250@gmail.com>
2025-03-20 14:07:53 +0200

    Save local changes before pulling

--- Choir-Frontend/src/App.jsx
+++ Choir-Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainLayout from "./Layouts/MainLayout.jsx";
 import Home from "./Pages/homePage.jsx";
-
+import Schedule from "./Pages/Schedule.jsx";
 
 const App = () => {
   return (
@@ -11,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home/>} />
+          <Route path="schedule" element={<Schedule />} />
         </Route>
       </Routes>
     </Router>
