9770c1cd02e23fad1820057b0f867357dc110824
DianeRUT <ingabirerdianna250@gmail.com>
2025-05-31 15:01:34 +0200

    corrected Hero.css path in MainLayout.jsx

--- Choir-Frontend/src/Layouts/MainLayout.jsx
+++ Choir-Frontend/src/Layouts/MainLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "../Components/Navbar.jsx";
 import Footer from "../Components/Footer.jsx";
 import { FaArrowDown, FaArrowUp } from "react-icons/fa";
-import "../styles/Hero.css";
+import "../styles/hero.css";
 
 const MainLayout = () => {
   const location = useLocation();
--- Choir-Frontend/src/Pages/homePage.jsx
+++ Choir-Frontend/src/Pages/homePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/Hero.css';
+import '../styles/hero.css';
 import choirImage from '../assets/logo1.jpg'; // Circular image
 
 const Hero = () => {
